bcc3d61b6628274d1c9d0d0426a5b40553124baf
ILGK-ADANTAS\acharron <alexandre.charron@illogika.com>
2017-11-11 16:29:51 -0500

    added bone lengths

--- Assets/PoseBuilder.cs
+++ Assets/PoseBuilder.cs
@@ -5,25 +5,21 @@ using UnityEngine;
 public class PoseBuilder : MonoBehaviour {
 
     public Pose p;
-    public float midChest;
-    public float shoulderWidth;
-    public float pelvis;
-    public float hipWidth;
 
     private void OnDrawGizmos()
     {
         Vector3 o = transform.position;
         Gizmos.color = Color.red;
         Gizmos.DrawLine(o + new Vector3(p.leftHand.x, p.leftHand.y, 0), o + new Vector3(p.leftElbow.x, p.leftElbow.y, 0));
-        Gizmos.DrawLine(o + new Vector3(p.leftElbow.x, p.leftElbow.y), o + new Vector3(-shoulderWidth, midChest, 0));
-        Gizmos.DrawLine(o + new Vector3(-shoulderWidth, midChest, 0), o + new Vector3(shoulderWidth, midChest, 0));
-        Gizmos.DrawLine(o + new Vector3(shoulderWidth, midChest, 0), o + new Vector3(p.rightElbow.x, p.rightElbow.y, 0));
+        Gizmos.DrawLine(o + new Vector3(p.leftElbow.x, p.leftElbow.y), o + new Vector3(-2, p.midChest, 0));
+        Gizmos.DrawLine(o + new Vector3(-2, p.midChest, 0), o + new Vector3(2, p.midChest, 0));
+        Gizmos.DrawLine(o + new Vector3(2, p.midChest, 0), o + new Vector3(p.rightElbow.x, p.rightElbow.y, 0));
         Gizmos.DrawLine(o + new Vector3(p.rightElbow.x, p.rightElbow.y, 0), new Vector3(p.rightHand.x, p.rightHand.y, 0));
-        Gizmos.DrawLine(o + new Vector3(0, midChest, 0), o + new Vector3(0, pelvis, 0));
+        Gizmos.DrawLine(o + new Vector3(0, p.midChest, 0), o + new Vector3(0, p.pelvis, 0));
         Gizmos.DrawLine(o + new Vector3(p.leftFoot.x, p.leftFoot.y, 0), o + new Vector3(p.leftKnee.x, p.leftKnee.y, 0));
-        Gizmos.DrawLine(o + new Vector3(p.leftKnee.x, p.leftKnee.y, 0), o + new Vector3(-hipWidth, pelvis, 0));
-        Gizmos.DrawLine(o + new Vector3(-hipWidth, pelvis, 0), o + new Vector3(hipWidth, pelvis, 0));
-        Gizmos.DrawLine(o + new Vector3(hipWidth, pelvis, 0), o + new Vector3(p.rightKnee.x, p.rightKnee.y, 0));
+        Gizmos.DrawLine(o + new Vector3(p.leftKnee.x, p.leftKnee.y, 0), o + new Vector3(-1, p.pelvis, 0));
+        Gizmos.DrawLine(o + new Vector3(-1, p.pelvis, 0), o + new Vector3(1, p.pelvis, 0));
+        Gizmos.DrawLine(o + new Vector3(1, p.pelvis, 0), o + new Vector3(p.rightKnee.x, p.rightKnee.y, 0));
         Gizmos.DrawLine(o + new Vector3(p.rightKnee.x, p.rightKnee.y, 0), o + new Vector3(p.rightFoot.x, p.rightFoot.y, 0));
 
 
--- Assets/script/Pose.cs
+++ Assets/script/Pose.cs
@@ -5,12 +5,38 @@ using System;
 
 [Serializable][CreateAssetMenu]
 public class Pose : ScriptableObject {
-    public Vector2 leftHand;
+    public Vector2 leftHand;    
     public Vector2 leftElbow;
-    public Vector2 rightHand;
+    public Vector2 rightHand;   
     public Vector2 rightElbow;
-    public Vector2 leftFoot;
+    public Vector2 leftFoot;   
     public Vector2 leftKnee;
-    public Vector2 rightFoot;
+    public Vector2 rightFoot;   
     public Vector2 rightKnee;
+
+    public float midChest;
+    public float pelvis;
+
+    [Header("Bone Lengths")]
+    public float leftForearmLength;
+    public float leftArmLength;
+    public float rightForearmLength;
+    public float rightArmLength;
+    public float leftShinLength;
+    public float leftThighLength;
+    public float RightShinLength;
+    public float RightThighLength;
+
+
+    void OnValidate()
+    {
+        leftForearmLength = Vector2.Distance(leftHand, leftElbow);
+        leftArmLength = Vector2.Distance(leftElbow, new Vector2(-2, midChest));
+        rightForearmLength = Vector2.Distance(rightHand, rightElbow);
+        rightArmLength = Vector2.Distance(rightElbow, new Vector2(2, midChest));
+        leftShinLength = Vector2.Distance(leftFoot, leftKnee);
+        leftThighLength = Vector2.Distance(leftKnee, new Vector2(-1, pelvis));
+        RightShinLength = Vector2.Distance(rightFoot, rightKnee);
+        RightThighLength = Vector2.Distance(rightKnee, new Vector2(1, pelvis));
+    }
 }
