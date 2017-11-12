aa803bc463c7fe0c9002237a27a589d7fb4b98e2
ILGK-ADANTAS\acharron <alexandre.charron@illogika.com>
2017-11-12 00:57:00 -0500

    added head to bonhomme

--- Assets/PoseDisplay.cs
+++ Assets/PoseDisplay.cs
@@ -14,6 +14,7 @@ public class PoseDisplay : MonoBehaviour {
     LineRenderer leftLeg;
     LineRenderer rightLeg;
     LineRenderer Body;
+    LineRenderer Head;
 
 	// Use this for initialization
 	void Start () {
@@ -52,6 +53,13 @@ public class PoseDisplay : MonoBehaviour {
         Body.endWidth = Body.startWidth = lineWidth;
         Body.numCornerVertices = Body.numCapVertices = extraVertices;
 
+        GameObject head = new GameObject("Head");
+        head.transform.parent = transform;
+        Head = head.AddComponent<LineRenderer>();
+        Head.sharedMaterial = m;
+        Head.endWidth = Head.startWidth = lineWidth * 2.0f;
+        Head.numCapVertices = extraVertices;
+
         leftArm.positionCount = 4;
         leftArm.startColor = Color.blue;
         leftArm.endColor = Color.red;
@@ -68,9 +76,13 @@ public class PoseDisplay : MonoBehaviour {
         rightLeg.startColor = Color.blue;
         rightLeg.endColor = Color.red;
 
-        Body.positionCount = 2;
+        Body.positionCount = 3;
         Body.startColor = Color.red;
         Body.endColor = Color.red;
+
+        Head.positionCount = 2;
+        Head.startColor = Color.red;
+        Head.endColor = Color.red;
     }
 
     public void UpdateDisplay(Pose p)
@@ -89,6 +101,8 @@ public class PoseDisplay : MonoBehaviour {
 
         rightLeg.SetPositions(new[] { o + new Vector3(p.rightFoot.x * c - p.rightFoot.y * s, p.rightFoot.y, 0), o + new Vector3(p.rightKnee.x * c - p.rightKnee.y * s, p.rightKnee.y, 0), o + new Vector3(-p.pelvis * s, p.pelvis * c, 0) });
 
-        Body.SetPositions(new[] { o + new Vector3(-p.pelvis * s, p.pelvis * c, 0), o + new Vector3(-p.midChest * s, p.midChest * c, 0) });
+        Body.SetPositions(new[] { o + new Vector3(-p.pelvis * s, p.pelvis * c, 0), o + new Vector3(-p.midChest * s, p.midChest * c, 0), o + new Vector3(-(p.midChest + 2) * s, (p.midChest + 2) * c, 0) });
+
+        Head.SetPositions(new[] { o + new Vector3(-(p.midChest + 2) * s, (p.midChest + 2) * c, 0), o + new Vector3(-(p.midChest + 3) * s, (p.midChest + 3) * c, 0) });
     }
 }
