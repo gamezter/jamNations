ea34c5be92d30de8e7605ba0cfdbc5db802a28cb
ILGK-ADANTAS\acharron <alexandre.charron@illogika.com>
2017-11-12 04:02:50 -0500

    added bubbles

--- Assets/PoseDisplay.cs
+++ Assets/PoseDisplay.cs
@@ -20,42 +20,54 @@ public class PoseDisplay : MonoBehaviour {
 	void Start () {
         GameObject leftA = new GameObject("Left arm");
         leftA.transform.parent = transform;
+        leftA.layer = LayerMask.NameToLayer("UI");
         leftArm = leftA.AddComponent<LineRenderer>();
+        leftArm.sortingOrder = 5;
         leftArm.sharedMaterial = m;
         leftArm.endWidth = leftArm.startWidth = lineWidth;
         leftArm.numCornerVertices = leftArm.numCapVertices = extraVertices;
 
         GameObject rightA = new GameObject("Right arm");
         rightA.transform.parent = transform;
+        rightA.layer = LayerMask.NameToLayer("UI");
         rightArm = rightA.AddComponent<LineRenderer>();
+        rightArm.sortingOrder = 5;
         rightArm.sharedMaterial = m;
         rightArm.endWidth = rightArm.startWidth = lineWidth;
         rightArm.numCornerVertices = rightArm.numCapVertices = extraVertices;
 
         GameObject leftL = new GameObject("Left leg");
         leftL.transform.parent = transform;
+        leftL.layer = LayerMask.NameToLayer("UI");
         leftLeg = leftL.AddComponent<LineRenderer>();
+        leftLeg.sortingOrder = 5;
         leftLeg.sharedMaterial = m;
         leftLeg.endWidth = leftLeg.startWidth = lineWidth;
         leftLeg.numCornerVertices = leftLeg.numCapVertices = extraVertices;
 
         GameObject rightL = new GameObject("Right leg");
         rightL.transform.parent = transform;
+        rightL.layer = LayerMask.NameToLayer("UI");
         rightLeg = rightL.AddComponent<LineRenderer>();
+        rightLeg.sortingOrder = 5;
         rightLeg.sharedMaterial = m;
         rightLeg.endWidth = rightLeg.startWidth = lineWidth;
         rightLeg.numCornerVertices = rightLeg.numCapVertices = extraVertices;
 
         GameObject body = new GameObject("Body");
         body.transform.parent = transform;
+        body.layer = LayerMask.NameToLayer("UI");
         Body = body.AddComponent<LineRenderer>();
+        Body.sortingOrder = 5;
         Body.sharedMaterial = m;
         Body.endWidth = Body.startWidth = lineWidth;
         Body.numCornerVertices = Body.numCapVertices = extraVertices;
 
         GameObject head = new GameObject("Head");
         head.transform.parent = transform;
+        head.layer = LayerMask.NameToLayer("UI");
         Head = head.AddComponent<LineRenderer>();
+        Head.sortingOrder = 5;
         Head.sharedMaterial = m;
         Head.endWidth = Head.startWidth = lineWidth * 2.0f;
         Head.numCapVertices = extraVertices;
@@ -92,17 +104,16 @@ public class PoseDisplay : MonoBehaviour {
         float s = Mathf.Sin(p.rotation);
         float c = Mathf.Cos(p.rotation);
 
-
         leftArm.SetPositions(new[] { o + new Vector3(p.leftHand.x * c - p.leftHand.y * s, p.leftHand.y * c + p.leftHand.x * s, 0), o + new Vector3(p.leftElbow.x * c - p.leftElbow.y * s, p.leftElbow.y * c + p.leftElbow.x * s, 0), o + new Vector3(-2 * c - p.midChest * s, p.midChest * c - 2 * s, 0), o + new Vector3( -p.midChest * s, p.midChest * c, 0) });
 
         rightArm.SetPositions(new[] { o + new Vector3(p.rightHand.x * c - p.rightHand.y * s, p.rightHand.y * c + p.rightHand.x * s, 0), o + new Vector3(p.rightElbow.x * c - p.rightElbow.x * s, p.rightElbow.y * c + p.rightElbow.x * s, 0), o + new Vector3(2 * c - p.midChest * s, p.midChest * c + 2 * s, 0), o + new Vector3(-p.midChest * s, p.midChest * c, 0) });
 
         leftLeg.SetPositions(new[] { o + new Vector3(p.leftFoot.x * c - p.leftFoot.y * s, p.leftFoot.y * c + p.leftFoot.x * s, 0), o + new Vector3(p.leftKnee.x * c - p.leftKnee.y * s, p.leftKnee.y * c + p.leftKnee.x * s, 0), o + new Vector3(-p.pelvis * s, p.pelvis * c, 0) });
 
-        rightLeg.SetPositions(new[] { o + new Vector3(p.rightFoot.x * c - p.rightFoot.y * s, p.rightFoot.y, 0), o + new Vector3(p.rightKnee.x * c - p.rightKnee.y * s, p.rightKnee.y, 0), o + new Vector3(-p.pelvis * s, p.pelvis * c, 0) });
+        rightLeg.SetPositions(new[] { o + new Vector3(p.rightFoot.x * c - p.rightFoot.y * s, p.rightFoot.y, 0), o + new Vector3(p.rightKnee.x * c - p.rightKnee.y * s, p.rightKnee.y, 0), o + new Vector3(-p.pelvis * s, p.pelvis * c, 0)});
 
-        Body.SetPositions(new[] { o + new Vector3(-p.pelvis * s, p.pelvis * c, 0), o + new Vector3(-p.midChest * s, p.midChest * c, 0), o + new Vector3(-(p.midChest + 2) * s, (p.midChest + 2) * c, 0) });
+        Body.SetPositions(new[] { o + new Vector3(-p.pelvis * s, p.pelvis * c, 0), o + new Vector3(-p.midChest * s, p.midChest * c, 0), o + new Vector3(-(p.midChest + 2) * s, (p.midChest + 2) * c, 0)});
 
-        Head.SetPositions(new[] { o + new Vector3(-(p.midChest + 2) * s, (p.midChest + 2) * c, 0), o + new Vector3(-(p.midChest + 3) * s, (p.midChest + 3) * c, 0) });
+        Head.SetPositions(new[] { o + new Vector3(-(p.midChest + 2) * s, (p.midChest + 2) * c, 0), o + new Vector3(-(p.midChest + 3) * s, (p.midChest + 3) * c, 0)});
     }
 }
