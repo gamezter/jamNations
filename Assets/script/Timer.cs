e5ae93867a286958aae5c6f8535a185f849c2680
ILGK-ADANTAS\acharron <alexandre.charron@illogika.com>
2017-11-11 23:26:54 -0500

    added scoring system

--- Assets/script/Timer.cs
+++ Assets/script/Timer.cs
@@ -9,7 +9,10 @@ public class Timer : MonoBehaviour {
     public Text DelayText;
     public PoseDisplay poseDisplay;
     public Pose[] poses;
+    public float maxDistance;
+    public float score;
 
+    public GameObject parent;
     public GameObject leftHandTarget;
     public GameObject leftElbowTarget;
     public GameObject rightHandTarget;
@@ -39,6 +42,32 @@ public class Timer : MonoBehaviour {
         {
             //times up
             delay = stagedelay;
+
+            score = 0;
+            Pose p = poses[currentIndex];
+            float c = Mathf.Cos(p.rotation);
+            float s = Mathf.Sin(p.rotation);
+
+            Vector3 leftHand = new Vector3(p.leftHand.x * s - p.leftHand.y * c, p.leftHand.y * s + p.leftHand.x * c, 0);
+            Vector3 leftElbow = new Vector3(p.leftElbow.x * s - p.leftElbow.y * c, p.leftElbow.y * s + p.leftElbow.x * c, 0);
+            Vector3 rightHand = new Vector3(p.rightHand.x * s - p.rightHand.y * c, p.rightHand.y * s + p.rightHand.x * c, 0);
+            Vector3 rightElbow = new Vector3(p.rightElbow.x * s - p.rightElbow.y * c, p.rightElbow.y * s + p.rightElbow.x * c, 0);
+            Vector3 leftKnee = new Vector3(p.leftKnee.x * s - p.leftKnee.y * c, p.leftKnee.y * s + p.leftKnee.x * c, 0);
+            Vector3 leftFoot = new Vector3(p.leftFoot.x * s - p.leftFoot.y * c, p.leftFoot.y * s + p.leftFoot.x * c, 0);
+            Vector3 rightKnee = new Vector3(p.rightKnee.x * s - p.rightKnee.y * c, p.rightKnee.y * s + p.rightKnee.x * c, 0);
+            Vector3 rightFoot = new Vector3(p.rightFoot.x * s - p.rightFoot.y * c, p.rightFoot.y * s + p.rightFoot.x * c, 0);
+
+            score += Mathf.Max(100 - (leftHand - leftHandTarget.transform.position + parent.transform.position).magnitude * 100 / maxDistance, 0.0f);
+            score += Mathf.Max(100 - (leftElbow - leftElbowTarget.transform.position + parent.transform.position).magnitude * 50 / maxDistance, 0.0f);
+            score += Mathf.Max(100 - (rightHand - rightHandTarget.transform.position + parent.transform.position).magnitude * 100 / maxDistance, 0.0f);
+            score += Mathf.Max(100 - (rightElbow - rightElbowTarget.transform.position + parent.transform.position).magnitude * 50 / maxDistance, 0.0f);
+            score += Mathf.Max(100 - (leftKnee - leftKneeTarget.transform.position + parent.transform.position).magnitude * 50 / maxDistance, 0.0f);
+            score += Mathf.Max(100 - (leftFoot - leftFootTarget.transform.position + parent.transform.position).magnitude * 100 / maxDistance, 0.0f);
+            score += Mathf.Max(100 - (rightKnee - rightKneeTarget.transform.position + parent.transform.position).magnitude * 50 / maxDistance, 0.0f);
+            score += Mathf.Max(100 - (rightFoot - rightFootTarget.transform.position + parent.transform.position).magnitude * 100 / maxDistance, 0.0f);
+
+            score /= 8;
+
             currentIndex = currentIndex == poses.Length - 1 ? 0 : currentIndex + 1;
             poseDisplay.UpdateDisplay(poses[currentIndex]);
         }
